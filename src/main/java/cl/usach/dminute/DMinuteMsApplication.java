b8efa8b01ab83b23f10e45df2768c7b2538db341
woliverhl <ohidalgoleal@gmail.com>
2018-06-15 00:27:13 -0400

    se sube configiracion config-server y eureka

--- src/main/java/cl/usach/dminute/DMinuteMsApplication.java
+++ src/main/java/cl/usach/dminute/DMinuteMsApplication.java
@@ -2,8 +2,10 @@ package cl.usach.dminute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "cl.usach.dminute")
+@EnableDiscoveryClient
 public class DMinuteMsApplication {
 
 	public static void main(String[] args) {
